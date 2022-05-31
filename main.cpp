e1d682bae6afdcd589b5b626e920dd25b35a049d
PhamDucThanh26 <100608850+PhamDucThanh26@users.noreply.github.com>
2022-05-31 14:52:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,20 +9,26 @@
 #include "Menu.h"
 #include "collision.h"
 #include "game.h"
-#include <SFML/Audio.hpp>
+
 using namespace std;
 
 int main() {
-    //MUSIC
-    sf::Music music;
-    if (!music.openFromFile("Res/Viet-Nam-Oi-Minh-Beta.wav"));
-    music.play();
 	//WINDOW
 	sf::RenderWindow game(sf::VideoMode(800, 600), "Game");
     Menu menu(game.getSize().x, game.getSize().y);
     Texture text;
     text.loadFromFile("Res/background.jpg");
     Sprite s(text);
+    Font font;
+    if (!font.loadFromFile("Res/Gotham-Bold.otf")){
+        std::cout << "Error";
+    }
+    Text t;
+    t.setFont(font);
+    t.setFillColor(Color::Blue);
+    t.setString("use your mouse to move and \n left mouse to shot the enemy . ");
+    t.setCharacterSize(30);
+    t.setPosition(50, 100);
 	while(game.isOpen()){
         Event event;
         while (game.pollEvent(event)){
@@ -38,8 +44,26 @@ int main() {
                 if (event.key.code == Keyboard::Return){
                     int x = menu.GetPressedItem();
                     if (x == 0){
-                         music.play();
                         play();
+                    }
+                    if (x==1){
+                        sf::RenderWindow howToPlay(sf::VideoMode(800, 600), "HTP");
+                        while (howToPlay.isOpen()){
+                            Event aevent;
+                            while (howToPlay.pollEvent(aevent)){
+                                if (aevent.type == Event::Closed)
+                                    howToPlay.close();
+                                if (aevent.type == Event::KeyPressed)
+                                    if (aevent.key.code == Keyboard::Escape)
+                                        howToPlay.close();
+                            }
+                            howToPlay.draw(s);
+                            howToPlay.draw(t);
+                            howToPlay.display();
+                        }
+                    }
+                    if (x==2){
+                        game.close();
                     }
                 }
             }
